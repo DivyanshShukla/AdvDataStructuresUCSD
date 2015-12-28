66f172e1eeda30f38992e86951638ee6edc98aee
Divyansh Shukla <divyansh16@gmail.com>
2015-12-29 00:48:09 +0530

    Comments added; commit to develop

--- src/Graph/Graph.java
+++ src/Graph/Graph.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public abstract class Graph {
 
+	// Properties common across all kinds of graphs
 	private int vertices;
 	private int edges;
 
@@ -17,6 +18,7 @@ public abstract class Graph {
 		this.edges = 0;
 	}
 
+	// methods specific for different graph implementations
 	public abstract void implementAddVertex();
 
 	public abstract List<Integer> getAllNeighbors();
